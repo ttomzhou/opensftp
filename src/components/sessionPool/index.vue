8c9c03be9248d434cc4118c16307a4982e95824b
邢荣 <xingrong.dev@gmail.com>
2021-02-12 18:55:28 +0800

    code async

---
+++ src/components/sessionPool/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: 'SessionPool',
+    components: {
+    },
+    data() {
+        return {
+        }
+    },
+    methods: {
+    },
+    created() {
+    },
+};
+</script>
